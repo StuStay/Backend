79baf5e521d4a068bdb2ce70a0193be55b82b07a
ons27 <80257884+ons27@users.noreply.github.com>
2023-11-14 05:53:03 +1200

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ import mongoose from 'mongoose';
 import payementsRoute from './routes/payementRoute.js'; 
 import reservationRoute from './routes/reservationRoute.js'
 import logementRoutes from './routes/logement.js';
+import reclamationRoutes from './routes/reclamation.js';
 
 const app = express();
 app.use(express.json());
 app.use('/api/payments', payementsRoute); 
 app.use('/api/reservations',reservationRoute);
 app.use('/logements', logementRoutes);
+app.use('/reclamations', reclamationRoutes);
 mongoose.connect('mongodb+srv://Stustay:Stustay4sim@stustay.a451zns.mongodb.net/?retryWrites=true&w=majority')
   .then(() => {
     console.log('Connected to MongoDB');
